e06bc2fcd71a8851cd8c9fa05d1aeb85bd3ef279
Antrussian <antonino.russo.agent@gmail.com>
2023-12-22 02:25:03 +0100

    Aggiungo i modules e creo una emit passando il dato in console

--- vite-boolflix-folder/src/App.vue
+++ vite-boolflix-folder/src/App.vue
@@ -1,13 +1,52 @@
 <template lang="">
   <div>
+    <!-- Componente per la ricerca dei film -->
+    <AppSearch @search="getMovies"/>
     
+    <!-- Componente principale dell'applicazione -->
+    <AppMain/>
   </div>
 </template>
+
 <script>
+// Importazione dei componenti e delle dipendenze necessarie
+import AppSearch from './components/AppSearch.vue';
+import AppMain from './components/AppMain.vue';
+import axios from 'axios';
+
 export default {
+  // Dichiarazione dei componenti utilizzati
+  components: {
+    AppSearch,
+    AppMain,
+  },
+
+  // Dichiarazione dei dati dello stato
+  data() {
+    return {
+      moviesList: [], // Lista dei film ottenuti dalla chiamata API
+    }
+  },
   
+  // Dichiarazione dei metodi dell'istanza
+  methods: {
+    // Metodo chiamato quando viene eseguita la ricerca
+    getMovies(searchContent) {
+      // Chiamata Axios per ottenere i film in base al contenuto di ricerca
+      console.log(searchContent);
+  
+    }
+  },
+
+  // Hook del ciclo di vita: creato quando l'istanza del componente è creata
+  created() {
+    console.log('App.vue created called');
+    // Chiamata iniziale per ottenere i film all'avvio dell'app
+    this.getMovies();
+  },
 }
 </script>
+
 <style lang="">
-  
+
 </style>
---
+++ vite-boolflix-folder/src/components/AppSearch.vue
@@ -0,0 +1,29 @@
+<template lang="">
+    <!-- Componente di ricerca: contiene un campo di input e un pulsante di ricerca -->
+    <div>
+      <!-- Campo di input legato a searchedString tramite v-model -->
+      <input type="text" v-model="searchedString">
+  
+      <!-- Pulsante di ricerca che emette l'evento 'search' con la stringa cercata -->
+      <button @click="$emit('search', searchedString)">search</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    // Nome del componente
+    name: 'AppSearch',
+  
+    // Dati dello stato del componente
+    data() {
+      return {
+        searchedString: '', // Stringa inserita dall'utente per la ricerca
+      }
+    },
+  }
+  </script>
+  
+  <style lang="">
+   
+  </style>
+  
